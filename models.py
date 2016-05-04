6540f4af79bfc2b5eaa1770cc0bbf2bf7808c2cf
Kirill Sevastyanenko <kirillseva@gmail.com>
2016-05-04 12:23:50 -0500

    pep8 doesn't like my space alignment

--- models.py
+++ models.py
@@ -7,7 +7,7 @@ from sqlalchemy.dialects.postgresql import JSONB
 class ModelGrid(db.Model):
     __tablename__ = 'model_grids'
 
-    id   = db.Column(db.String(), primary_key=True)
+    id = db.Column(db.String(), primary_key=True)
     grid = db.Column(JSONB)
 
     def __init__(self, id, grid):
